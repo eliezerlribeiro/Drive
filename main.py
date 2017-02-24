c3c5169a17000f79821afc1d14273ccaa30cfae5
wdalmorra <wddsouza@inf.ufpel.edu.br>
2017-02-24 15:14:08 -0300

    Produtor/Consumidor/Contatenador semi construidos. Faltam detalhes, e entao aplicar multithreading

--- main.py
+++ main.py
@@ -1,11 +1,73 @@
 from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
+import numpy as np
+import sys
 
 
-thread.start(NProdutores, NFiles, NValoresRandomicos );
-thread.start(Nconsumidores);
-thread.start(Concatenador);
+def produtor(id, n_files, size_file, drive):
+	
+	for x in range(n_files):
+		file = drive.CreateFile({'title': str(id) + str(x) + '.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+		content = ''
+		for y in range(size_file):
+			content += str(np.random.randint(0, 100)) + " "
 
-print 'FIM'
+		file.SetContentString(content) # Set content of the file from given string.
+		file.Upload()
+
+
+def consumidor(drive):
+
+	file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+	for file1 in file_list:
+
+		if file1['title'][-1] != '_':
+	 		# print('title: %s, id: %s' % (file1['title'], file1.GetContentString()))
+
+	 		numbers = file1.GetContentString().split(' ')[:-1]
+
+	 		file_content = [ int(n) for n in numbers]
+
+	 		sorted_file = np.sort(file_content)
+
+			file1['title'] = file1['title'] + "_"
+			file1.Upload()
+
+def concatenador(drive):
+
+	final_file = drive.CreateFile({'title': 'Concatenado.txt'})
+
+	file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+
+	for file1 in file_list:
+
+		if file1['title'][-1] == '_':
+	 		# print('title: %s, id: %s' % (file1['title'], file1.GetContentString()))
+
+	 		numbers = file1.GetContentString().split(' ')[:-1]
+	 		
+	 		file_content = [ int(n) for n in numbers]
+
+	 		sorted_file = np.sort(file_content)
+
+			file1['title'] = file1['title'] + "_"
+			file1.Upload()
+
+
+
+def main(argv):
+
+	gauth = GoogleAuth()
+	gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
+	drive = GoogleDrive(gauth)
+
+	produtor(1, 2, 10, drive)
+	consumidor(drive)
+	print 'FIM'
+
+
+
+if __name__ == '__main__':
+	main(sys.argv)
 
 
