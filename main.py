096a329215ed25591af1053e1af6205a91f539e4
wdalmorra <wddouza@inf.ufpel.edu.br>
2017-02-24 19:47:28 -0800

    Threads criadas, produtores funcionais. Necessita analisar sem6antica dos consumidores e do concatenador.

--- main.py
+++ main.py
@@ -2,67 +2,136 @@ from pydrive.drive import GoogleDrive
 from pydrive.auth import GoogleAuth
 import numpy as np
 import sys
+import threading
 
 
-def produtor(id, n_files, size_file, drive):
-	
-	for x in range(n_files):
-		file = drive.CreateFile({'title': str(id) + str(x) + '.txt'})  # Create GoogleDriveFile instance with title 'Hello.txt'.
-		content = ''
-		for y in range(size_file):
-			content += str(np.random.randint(0, 100)) + " "
+class Produtor(threading.Thread):
+	"""docstring for Produtor"""
+	def __init__(self, idp, n_files, size_file, drive, contador, fila):
+		threading.Thread.__init__(self)
+		self.idp = idp
+		self.n_files = n_files
+		self.size_file = size_file
+		self.drive = drive
+		self.contador = contador
+		self.fila = fila
+		# self.http = self.drive.auth.Get_Http_Object()
 
-		file.SetContentString(content) # Set content of the file from given string.
-		file.Upload()
+	def run(self):
+		for x in range(self.n_files):
+			file_name = 'p' + str(self.idp) + 'f' + str(x) + '.txt'
+			file = self.drive.CreateFile({'title': file_name})  # Create GoogleDriveFile instance with title 'Hello.txt'.
+			content = ''
+			for y in range(self.size_file):
+				content += str(np.random.randint(0, sys.maxint)) + " "
 
+			file.SetContentString(content) # Set content of the file from given string.
+			file.Upload()
+			self.fila.SetContentString(self.fila.GetContentString() + ' ' + file_name)
+			self.fila.Upload()
 
-def consumidor(drive):
+class Consumidor(threading.Thread):
+	"""docstring for Produtor"""
+	def __init__(self, drive):
+		threading.Thread.__init__(self)
+		self.drive = drive
 
-	file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
-	for file1 in file_list:
+	def run(self):
+		file_list = self.drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+		for file1 in file_list:
 
-		if file1['title'][-1] != '_':
-	 		# print('title: %s, id: %s' % (file1['title'], file1.GetContentString()))
+			if file1['title'][-1] != '_':
+		 		# print('title: %s, id: %s' % (file1['title'], file1.GetContentString()))
 
-	 		numbers = file1.GetContentString().split(' ')[:-1]
+		 		numbers = file1.GetContentString().split(' ')[:-1]
 
-	 		file_content = [ int(n) for n in numbers]
+		 		file_content = [ int(n) for n in numbers]
 
-	 		sorted_file = np.sort(file_content)
+		 		sorted_file = np.sort(file_content)
 
-			file1['title'] = file1['title'] + "_"
-			file1.Upload()
+		 		content = ''
 
-def concatenador(drive):
+		 		for i in sorted_file:
+		 			content += str(i) + ' '
 
-	final_file = drive.CreateFile({'title': 'Concatenado.txt'})
+		 		print content
+		 		file1.SetContentString(content)
 
-	file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
+				file1['title'] = file1['title'] + "_"
+				file1.Upload()
 
-	for file1 in file_list:
+class Concatenador(threading.Thread):
+	"""docstring for Produtor"""
+	def __init__(self, drive):
+		threading.Thread.__init__(self)
+		self.drive = drive
 
-		if file1['title'][-1] == '_':
-	 		# print('title: %s, id: %s' % (file1['title'], file1.GetContentString()))
+	def run(self):
 
-	 		numbers = file1.GetContentString().split(' ')[:-1]
-	 		
-	 		file_content = [ int(n) for n in numbers]
+		final_file = drive.CreateFile({'title': 'Concatenado.txt'})
+		final_file.Upload()
 
-	 		sorted_file = np.sort(file_content)
+		file_list = drive.ListFile({'q': "'root' in parents and trashed=false"}).GetList()
 
-			file1['title'] = file1['title'] + "_"
-			file1.Upload()
+		for file1 in file_list:
+
+			if file1['title'][-1] == '_':
+		 		# print('title: %s, id: %s' % (file1['title'], file1.GetContentString()))
+
+		 		numbers = file1.GetContentString().split(' ')[:-1]
+		 		
+		 		file_content = [ int(n) for n in numbers]
+
+		 		old = final_file.GetContentString().split(' ')[:-1]
+
+		 		old_int = [ int(n) for n in old]
+
+		 		sorted_file = np.sort(file_content + old_int)
+
+		 		content = ''
+
+		 		for i in sorted_file:
+		 			content += str(i) + ' '
+
+		 		file1.Delete()
+
+		 		final_file.SetContentString(content)
+
+				final_file.Upload()
 
 
 
 def main(argv):
 
+	n_produtores = int(argv[1])
+	n_consumidores = int(argv[2])
+	n_files = int(argv[3])
+	file_size = int(argv[4])
+
 	gauth = GoogleAuth()
 	gauth.LocalWebserverAuth() # Creates local webserver and auto handles authentication.
 	drive = GoogleDrive(gauth)
 
-	produtor(1, 2, 10, drive)
-	consumidor(drive)
+	contador = drive.CreateFile({'title': 'Contador.txt'})
+	contador.SetContentString(str(n_files*n_produtores))
+	contador.Upload()
+
+	fila = drive.CreateFile({'title': 'Fila.txt'})
+	fila.Upload()
+
+	produtores = []
+
+	try:
+		for x in range(n_produtores):
+			produtores.append(Produtor(x, n_files, file_size, drive, contador, fila))
+			produtores[x].start()
+	except:
+		print 'Unable to start Thread' 
+
+
+	for x in range(n_produtores):
+		produtores[x].join()
+
 	print 'FIM'
 
 
